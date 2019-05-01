18c17e24f238e32610328dc14ceb4ff5b65df751
gy0109 <1974326896@qq.com>
2019-05-01 16:09:27 +0800

    重构postlist视图

--- 07-开发面向用户的界面/typeidea/typeidea/blog/models.py
+++ 07-开发面向用户的界面/typeidea/typeidea/blog/models.py
@@ -68,6 +68,36 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    # 重构post_list视图
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except tag.DoesNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+
+        return post_list, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category = None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+
+        return post_list, category
+
+    @classmethod
+    def latest_posts(cls):
+        queryset = cls.objects.filter(status=Post.STATUS_NORMAL)
+
+
 
 """
 常用字段介绍：
--- 07-开发面向用户的界面/typeidea/typeidea/blog/views.py
+++ 07-开发面向用户的界面/typeidea/typeidea/blog/views.py
@@ -13,22 +13,12 @@ def post_list(request, category_id=None, tag_id=None):
     category = None
 
     if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)       # 多对多的情况下 实现关联取值
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+       post_list , tag = Post.get_by_tag(tag_id)
 
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
     else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            try:
-                category = Category.objects.get(id=category_id)
-            except Category.DoesNotExist:
-                category = None
-            else:
-                post_list = post_list.filter(category_id=category_id)
+        post_list = Post.latest_posts()
 
     return HttpResponse(tem.render(locals()))
 

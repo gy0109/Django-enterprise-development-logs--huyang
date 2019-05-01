5c7d75df79abe1ea8acd907ed80ee74512a63030
gy0109 <1974326896@qq.com>
2019-05-01 16:13:22 +0800

    分类信息实现全部显示

--- 07-开发面向用户的界面/typeidea/typeidea/blog/models.py
+++ 07-开发面向用户的界面/typeidea/typeidea/blog/models.py
@@ -23,6 +23,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def get_navs(cls):
+        categories = cls.objects.filter(status=Post.STATUS_NORMAL)
+        nav_categories = categories.filter(is_nav=True)
+        normal_categories = categories.filter(is_nav=False)
+        return {
+            'navs': nav_categories,
+            'categories': normal_categories,
+        }
+
 
 class Tag(models.Model):
     # STATUS_NORMAL = 1

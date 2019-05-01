a5073da656c09a009cbb264ae7033523e911b0a5
gy0109 <1974326896@qq.com>
2019-05-01 14:59:23 +0800

    模板响应出不识闲

--- 07-开发面向用户的界面/typeidea/typeidea/blog/views.py
+++ 07-开发面向用户的界面/typeidea/typeidea/blog/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template.loader import get_template
 
 # Create your views here.
 
 
 def post_list(request, category_id=None, tag_id=None):
-    # content = 'post_list category_id={category_id}, tag_id={tag_id}' .format(category_id=category_id, tag_id=tag_id)
-    # return HttpResponse(content)
-    # reder(reqeust, template_name, context, xontent_type, ostatus, using使用哪种模板引擎)
-    return render(request, 'blog/list.html', context={'name': 'post_list'})
+    tem = get_template('list.html')
+    name = 'post_list'
+    return HttpResponse(tem.render(locals()))
 
 
 def post_detail(request, post_id):
-    return render(request, 'blog/detail.html', context={'name': 'post_list'})
-
+    tem = get_template('list.html')
+    name = 'post_list'
+    return HttpResponse(tem.render(locals()))
 

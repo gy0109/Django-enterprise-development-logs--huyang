ac87b8ca2bf2a94c0e529e2ce8c1e3b6ca7ebd32
gy0109 <1974326896@qq.com>
2019-05-05 19:49:29 +0800

    评论窗口的初步实现

--- 09-完成个人博客系统/typeidea/typeidea/comment/views.py
+++ 09-完成个人博客系统/typeidea/typeidea/comment/views.py
@@ -1,15 +1,30 @@
-# from django.shortcuts import render
-# from django.views.generic import DetailView
-# # Create your views here.
-# from blog.views import CommonViewMixin
-# from blog.models import Post
-#
-# from .forms import CommentForm
-#
-#
-# class PostDetailView(CommonViewMixin, DetailView):
-#     queryset = Post.latest_posts()
-#     template_name = 'blog/detail.html'
-#     context_object_name = 'post'
-#     pk_url_kwarg = 'post_id'
-#
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+# Create your views here.
+from .forms import CommentForm
+
+
+class CommentView(TemplateView):
+    http_method_names = ['post']
+    template_name = 'comment/result.html'
+
+    def post(self, request, *args, **kwargs):
+        comment_form = CommentForm(request.POST)
+        target = request.POST.get('target')
+
+        if comment_form.is_valid():
+            instance = comment_form.save(commit=False)
+            instance.target = target
+            instance.save()
+            succeed = True
+            return redirect(target)
+        else:
+            succeed = False
+
+        context = {
+            'succeed': succeed,
+            'form': comment_form,
+            'target': target
+        }
+        return self.render_to_response(context)
+

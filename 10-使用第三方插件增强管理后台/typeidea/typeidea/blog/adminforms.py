2cb1c80e7f60bc73bc45fce33ce9f99b40faf745
gy0109 <1974326896@qq.com>
2019-05-07 14:01:28 +0800

    form完善 js文件写入

--- 10-使用第三方插件增强管理后台/typeidea/typeidea/blog/adminforms.py
+++ 10-使用第三方插件增强管理后台/typeidea/typeidea/blog/adminforms.py
@@ -19,16 +19,40 @@ class PostAdminForm(forms.ModelForm):
         widget=autocomplete.ModelSelect2Multiple(url='tagautocomplete'),
         label='标签'
     )
-    # content = forms.CharField(widget=CKEditorWidget(), label='正文', required=True)
+    content = forms.CharField(widget=forms.HiddenInput(), label='正文', required=True)
+    content_md = forms.CharField(widget=forms.Textarea(), label='正文', required=True)
     # 提供图片加载
-    content = forms.CharField(widget=CKEditorUploadingWidget(), label='正文', required=True)
+    content_ck = forms.CharField(widget=CKEditorUploadingWidget(), label='正文', required=True)
 
     class Meta:
         model = Post
-        fields = ('category', 'tag', 'title', 'descripition', 'content', 'status')
+        fields = ('category', 'tag', 'title', 'descripition', 'is_md', 'content', 'content_ck', 'content_md', 'status')
 
+    def __init__(self, instance=None, initial=None, **kwargs):
+        initial = initial or ()
+        if instance:
+            if instance.is_md:
+                initial['content_md'] = instance.content
+            else:
+                initial['content_ck'] = instance.content
+        super().__init__(instance=instance, initial=initial, **kwargs)
 
+    def clean(self):
+        is_md = self.cleaned_data.get('is_md')
+        if is_md:
+            content_field_name = 'contend_md'
+        else:
+            content_field_name = 'contend_mk'
+        content = self.cleaned_data.get(content_field_name)
+        if not content:
+            self.add_error(content_field_name, '必填项')
+            return
 
+        self.cleaned_data['content'] = content
+        return super().clean()
+
+    class Media:
+        js = ('js/post_editor.js')
 
 
 

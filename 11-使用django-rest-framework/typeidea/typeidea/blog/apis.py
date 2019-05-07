fdc1e977f939d2a7f9fd312c97eb23b9f64a6a64
gy0109 <1974326896@qq.com>
2019-05-07 16:11:44 +0800

    定义接口

--- 11-使用django-rest-framework/typeidea/typeidea/blog/apis.py
+++ 11-使用django-rest-framework/typeidea/typeidea/blog/apis.py
@@ -1,22 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer
 
 
-@api_view()    # api_view 快速提供api接口  [’GET', 'POST]指定方法
-def post_list(request):
-    posts = Post.objects.filter(status=Post.STATUS_NORMAL)
-    post_serializers = PostSerializer(posts, many=True)
-    return Response(post_serializers.data)
+# @api_view()    # api_view 快速提供api接口  [’GET', 'POST]指定方法
+# def post_list(request):
+#     posts = Post.objects.filter(status=Post.STATUS_NORMAL)
+#     post_serializers = PostSerializer(posts, many=True)
+#     return Response(post_serializers.data)
+#
+#
+# # ListCreateAPIView 和LISTView类似  需要提供queryset参数  数据列表页
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+#     serializer_class = PostSerializer
 
 
-# ListCreateAPIView 和LISTView类似  需要提供queryset参数  数据列表页
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+# 基础的数据操作接口
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
-
-
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    # permission_classes = [IsAdminUser]   # 权限  写入
 
--- 11-使用django-rest-framework/typeidea/typeidea/blog/urls.py
+++ 11-使用django-rest-framework/typeidea/typeidea/blog/urls.py
@@ -1,10 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib.sitemaps import views as sitemap_views
 from blog.rss import LatestPostFeed
 from blog.sitemap import PostSitemap
 from . import views
 from .autocomplete import CategoryAutoComplete, TagAutoComplete
 from . import apis
+from rest_framework.routers import DefaultRouter
+
+# base_name可以代替namespace进行reverse操作
+router = DefaultRouter()
+router.register(r'post', apis.PostViewSet, base_name='api-post')
 
 urlpatterns = [
     # 博客首页：  https://www.gy.com
@@ -44,6 +49,7 @@ urlpatterns = [
     #  api/post/?format=json将数据一json格式输出
     url(r'^api/post/$', apis.PostList.as_view(), name='post_list'),
 
+    url(r'^api/', include(router.urls, ))  # namespace='api'rest_framework中有的地方并不支持recversehe recverse_acthion  所以使用router
 
 
     ]
